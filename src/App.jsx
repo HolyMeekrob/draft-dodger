d9e1bf4993c8f5955be03f6a04bf90d8a70b82cd
Holy Meekrob <holy.meekrob@gmail.com>
2018-08-24 18:59:18 -0500

    Convert spaces to tabs

--- src/App.jsx
+++ src/App.jsx
@@ -7,48 +7,48 @@ import Initialize from './components/Initialize/Initialize';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isInitialized: false
-    };
+	constructor() {
+		super();
+		this.state = {
+			isInitialized: false
+		};
 
-    this.onStart = this.onStart.bind(this);
-  }
+		this.onStart = this.onStart.bind(this);
+	}
 
-  onStart(teams) {
-    this.setState({
-      isInitialized: true,
-      teams,
-      nextPick: 1
-    });
-  }
+	onStart(teams) {
+		this.setState({
+			isInitialized: true,
+			teams,
+			nextPick: 1
+		});
+	}
 
-  render() {
-    const renderDraft = () => {
-      return (
-        <React.Fragment>
-          <Draft teams={this.state.teams} players={data.players} nextPick={this.state.nextPick} />
-        </React.Fragment>
-      );
-    };
+	render() {
+		const renderDraft = () => {
+			return (
+				<React.Fragment>
+					<Draft teams={this.state.teams} players={data.players} nextPick={this.state.nextPick} />
+				</React.Fragment>
+			);
+		};
 
-    const renderInitialize = () => (<div><Initialize onStart={this.onStart}/></div>);
+		const renderInitialize = () => (<div><Initialize onStart={this.onStart}/></div>);
 
-    const content = this.state.isInitialized
-      ? renderDraft()
-      : renderInitialize();
-    return (
-      <div className="app">
-        <header>
-          <h1>Draft Dodger</h1>
-        </header>
-        <ul>
-          {content}
-        </ul>
-      </div>
-    );
-  }
+		const content = this.state.isInitialized
+			? renderDraft()
+			: renderInitialize();
+		return (
+			<div className="app">
+				<header>
+					<h1>Draft Dodger</h1>
+				</header>
+				<ul>
+					{content}
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default App;
