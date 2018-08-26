c39a72182dbd33b92c3d7f74dff972560e3c8f2a
Holy Meekrob <holy.meekrob@gmail.com>
2018-08-26 14:43:10 -0500

    Reorder teams

--- src/components/Initialize/Initialize.jsx
+++ src/components/Initialize/Initialize.jsx
@@ -13,6 +13,8 @@ class Initialize extends Component {
 		this.handleNameChange = this.handleNameChange.bind(this);
 		this.handleOwnerChange = this.handleOwnerChange.bind(this);
 		this.handleAddClick = this.handleAddClick.bind(this);
+		this.handleMoveUpClick = this.handleMoveUpClick.bind(this);
+		this.handleMoveDownClick = this.handleMoveDownClick.bind(this);
 		this.handleStartClick = this.handleStartClick.bind(this);
 	}
 
@@ -20,7 +22,7 @@ class Initialize extends Component {
 		this.setState({name: event.target.value});
 	}
 
-	handleAddClick(event) {
+	handleAddClick() {
 		this.setState(prevState => {
 			const newTeam = {
 				id: prevState.teams.length + 1,
@@ -31,6 +33,52 @@ class Initialize extends Component {
 				name: '',
 				teams: prevState.teams.concat(newTeam),
 				errorMessage: ''
+			};
+		});
+	}
+
+	handleMoveUpClick(event) {
+		const teamId = parseInt(event.target.parentNode.dataset.teamId, 10);
+		this.setState(prevState => {
+			const teams = prevState.teams.map(team => {
+				const id = team.id === teamId
+					? teamId - 1
+					: team.id === teamId - 1 ? teamId : team.id;
+
+				return {
+					...team,
+					id
+				};
+			});
+
+			teams.sort((a, b) => a.id - b.id);
+
+			return {
+				...prevState,
+				teams
+			};
+		});
+	}
+
+	handleMoveDownClick(event) {
+		const teamId = parseInt(event.target.parentNode.dataset.teamId, 10);
+		this.setState(prevState => {
+			const teams = prevState.teams.map(team => {
+				const id = team.id === teamId
+					? teamId + 1
+					: team.id === teamId + 1 ? teamId : team.id;
+
+				return {
+					...team,
+					id
+				};
+			});
+
+			teams.sort((a, b) => a.id - b.id);
+
+			return {
+				...prevState,
+				teams
 			};
 		});
 	}
@@ -66,14 +114,24 @@ class Initialize extends Component {
 	}
 
 	render() {
-		const error = this.state.errorMessage.length > 0
-			? (<div className="error">{this.state.errorMessage}</div>)
+		const { errorMessage, name, teams } = this.state;
+
+		const error = errorMessage.length > 0
+			? (<div className="error">{errorMessage}</div>)
 			: null;
 
-		const teams = this.state.teams.map(team => {
+		const teamList = teams.map(team => {
+			const moveUp = team.id === 1
+				? null
+				: <button type="button" onClick={this.handleMoveUpClick}>&#x2BC5;</button>;
+
+			const moveDown = team.id === teams.length
+				? null
+				: <button type="button" onClick={this.handleMoveDownClick}>&#x2BC6;</button>;
+
 			return (
-				<li key={team.id}>
-					{team.name}
+				<li key={team.id} data-team-id={team.id}>
+					{moveUp} {moveDown} {team.name}
 					<input type="radio" name="is-mine" value={team.id} checked={team.isOwned} onChange={this.handleOwnerChange} />
 				</li>
 			);
@@ -83,14 +141,14 @@ class Initialize extends Component {
 				{error}
 				<div>
 					<label htmlFor="new-team">New team: </label>
-					<input id="new-team" type="text" value={this.state.name} onChange={this.handleNameChange} />
+					<input id="new-team" type="text" value={name} onChange={this.handleNameChange} />
 				</div>
 				<div>
 					<button type="button" onClick={this.handleAddClick}>Add team</button>
 				</div>
 				<hr />
 				<div>
-					<ul>{teams}</ul>
+					<ol>{teamList}</ol>
 				</div>
 				<hr />
 				<button type="button" onClick={this.handleStartClick}>Start draft</button>
